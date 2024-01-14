1d5e228c3a6df4020fbfdace8170f7ec4a91051c
Shivam <shivamlather5@gmail.com>
2024-01-14 10:36:03 -0500

    Made navbar dynamic

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,43 +2,43 @@ import React from 'react'
 import { Navbar } from './Components/Navbar'
 import { Card } from './Components/Card'
 import { Signup } from './Components/Signup'
-import { BrowserRouter as Router, Route, Link,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link,Routes, useLocation } from 'react-router-dom';
 import { UserSignin } from './Components/Signin';
 import { AdminSignin } from './Components/Admin';
 import { ShowCourse } from './Components/ShowCourse';
 import { AddCourse } from './Components/AddCourse';
+import { AdminNavbar } from './AdminNavBar';
+
 
 function App() {
+  const location = useLocation();
   return (
     <>
-    
-    <nav>
-      <div className="bg-slate-800 w-vw h-20 flex justify-between items-center">
-        <div>
-          <span className="text-white text-3xl font-bold ml-16 cursor-pointer">Dukan</span>
-        </div>
-        <div><ul className="flex text-white w-[350px] justify-between mr-48 cursor-pointer font-semibold text-lg">
-              <li>
-                <Link to="/">Sign up</Link>
-              </li>
-              <li>
-                <Link to="/Signin">Sign in</Link>
-              </li>
-              <li>
-                <Link to="/Admin">Admin</Link>
-              </li>
-            </ul></div>
-      </div>
-      </nav>
-    
+      {/* Conditionally render Navbar only if not on /AdminAddCourse route */}
+      {location.pathname !== '/AdminAddCourse' && <Navbar />}
+
       <Routes>
-        <Route path="/" element={<Card comp={<Signup></Signup>}></Card>}></Route>
-        <Route path="/Signin" element={<Card comp={<UserSignin></UserSignin>}></Card>}></Route>
-        <Route path="/Admin" element={<Card comp={<AdminSignin></AdminSignin>}></Card>}></Route>
-        <Route path='/ShowCourse' element={<ShowCourse></ShowCourse>}></Route>
-        <Route path='/AdminAddCourse' element={<Card comp={<AddCourse></AddCourse>}></Card>}></Route>
+        <Route path="/" element={<Card comp={<Signup />} />}></Route>
+        <Route
+          path="/Signin"
+          element={<Card comp={<UserSignin />} />}
+        ></Route>
+        <Route
+          path="/Admin"
+          element={<Card comp={<AdminSignin />} />}
+        ></Route>
+        <Route path="/ShowCourse" element={<ShowCourse />} />
+        <Route
+          path="/AdminAddCourse"
+          element={
+            <>
+            <AdminNavbar></AdminNavbar>
+            <Card comp={<AddCourse></AddCourse>}></Card>
+            </>
+          }
+        ></Route>
       </Routes>
-   </>
+    </>
   )
 }
 
--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,28 +1,42 @@
-import { BrowserRouter as Link } from 'react-router-dom';
-import { UserSignin } from './Signin';
-import { AdminSignin } from './Admin';
-import { Signup } from "./Signup";
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-export function Navbar() {
+export function Navbar(){
+  const location = useLocation();
+
   return (
-    
-    <nav>
-      <div className="bg-slate-800 w-vw h-20 flex justify-between items-center">
-        <div>
-          <span className="text-white text-3xl font-bold ml-16 cursor-pointer">Dukan</span>
-        </div>
-        <div><ul className="flex text-white w-[150px] justify-between mr-48 cursor-pointer font-semibold text-lg">
-              <li>
-                <Link to="/">Signup</Link>
-              </li>
-              <li>
-                <Link to="/Signin">Signin</Link>
-              </li>
-              <li>
-                <Link to="/Admin">Admin</Link>
-              </li>
-            </ul></div>
+    <div className="bg-slate-800 w-vw h-20 flex justify-between items-center">
+      <div>
+        <span className="text-white text-3xl font-bold ml-16 cursor-pointer">
+          Dukan
+        </span>
       </div>
-      </nav>
+      <div>
+        <ul className="flex text-white w-[350px] justify-between mr-48 cursor-pointer font-semibold text-lg">
+          <li>
+            <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+              Sign up
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/Signin"
+              className={location.pathname === '/Signin' ? 'active' : ''}
+            >
+              Sign in
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/Admin"
+              className={location.pathname === '/Admin' ? 'active' : ''}
+            >
+              Admin
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
-}
+};
+
