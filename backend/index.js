0244c9f8862cb28a173c650140cb8e1b9a73e2ee
Shivam <shivamlather5@gmail.com>
2024-01-14 06:13:07 -0500

    Added signin api

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@ const cors = require('cors')
 const {Info} = require('./Database')
 const {valid} = require('./Validation')
 const app = express();
+const bodyParser = require('body-parser')
 
 app.use(cors())
 app.use(express.json())
+app.use(bodyParser.json());
 
-app.post("/user",async (req,res)=>{
+app.post("/signup",async (req,res)=>{
     const response = valid.safeParse(req.body)
     if(!response.success){
         res.status(400).json({
@@ -31,5 +33,28 @@ app.post("/user",async (req,res)=>{
         msg:"user has been created"
     })
 })
+
+app.post("/signinuser", async (req, res) => {
+    const { username, password } = req.body;
+  
+    try {
+        const existingUser = await Info.findOne({ username: username, password: password });
+  
+      if (existingUser) {
+        res.status(200).json({
+          msg: "User exists"
+        });
+      } else {
+        res.status(400).json({
+          msg: "User not found"
+        });
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      res.status(500).json({
+        msg: "Internal Server Error"
+      });
+    }
+  });
 
 app.listen(3000)
